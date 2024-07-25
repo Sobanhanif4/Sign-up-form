0f02bdaf003731823bf3217c7ff96bc3a325c519
Soban hanif <sobanatban4@gmail.com>
2024-07-25 22:24:17 +0500

    added changes

---
+++ main.js
@@ -0,0 +1,8 @@
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let name = document.getElementById("name");
+let title = document.getElementById("title");
+
+signinBtn.onclick = function () {
+    nameField.style.maxHeight = '0';
+}
